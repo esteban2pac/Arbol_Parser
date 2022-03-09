925b47828aa6a96c9a64ff88bd2fef65044d81cb
esteban2pac <90542009+esteban2pac@users.noreply.github.com>
2022-03-09 12:48:19 -0500

    Presentable

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 import xml.etree.ElementTree as ET
 
-
 class parserXmlToPy(ttk.Frame):
 
     def __init__(self, main_window):
@@ -10,23 +9,23 @@ class parserXmlToPy(ttk.Frame):
         super().__init__(main_window)
         main_window.title("Arbol XML")
         main_window.geometry("500x500")
-        self.treeview = ttk.Treeview(self)
-        self.tree = ET.parse('Cartelera_Cine.xml')
-        self.raiz = self.tree.getroot()
-        self.nodo_principal = self.treeview.insert("", END, None, text=self.raiz.tag)
-        self.insert_treeview(self.raiz, parent=self.nodo_principal)
-        self.treeview.pack(fill=BOTH, expand=True)
+        self.vistaArbol = ttk.Treeview(self)
+        self.arbol = ET.parse('Cartelera_Cine.xml')
+        self.raiz = self.arbol.getroot()
+        self.nodo_principal = self.vistaArbol.insert("", END, None, text=self.raiz.tag)
+        self.insertando_vistaArbol(self.raiz, parent=self.nodo_principal)
+        self.vistaArbol.pack(fill=BOTH, expand=True)
         self.pack(fill=BOTH, expand=True)
 
-    def insert_treeview(self, raiz, parent=""):
+    def insertando_vistaArbol(self, raiz, parent=""):
         for child in raiz:
             node_name = child.tag
-            item = self.treeview.insert(parent, END, None, text=node_name)
+            item = self.vistaArbol.insert(parent, END, None, text=node_name)
             if len(child) > 0:
-                self.insert_treeview(child, parent=item)
+                self.insertando_vistaArbol(child, parent=item)
             else:
                 if (child.text != None):
-                    self.treeview.insert(item, END, None, text=child.text)
+                    self.vistaArbol.insert(item, END, None, text=child.text)
 
 
 root = Tk()
